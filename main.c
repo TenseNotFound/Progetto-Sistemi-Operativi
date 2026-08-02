243fae147059c0d626a8cb648356c0e66f694e48
LorenzoTarantino <lorenzotarantino40@gmail.com>
2026-08-02 11:17:44 +0200

    Inserimento librerie per standard Posix

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
+/*
+    POSIX RELEASE
+*/
+
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/mman.h>
+#include <sys/sem.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
     return 0;
