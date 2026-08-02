dcf51a56fe1209eb6763e63316fc1a8e95c2a891
LorenzoTarantino <lorenzotarantino40@gmail.com>
2026-08-02 16:05:07 +0200

    inserimento di protocollo.h contenente le varie tipologie di gam_info e l'azione (con tutti le possibili situazioni che si possono verificare) del client durante la partita

---
+++ protocollo/protocollo.h
@@ -0,0 +1,11 @@
+typedef enum game_info{ // serve poi per capire che mossa/azione è stata fatta/compiuta/subita sul client
+    WELCOME, JOIN, TURN, MOVE, HIT, MISS, ELIMINATED, WIN
+}game_info;
+
+
+typedef struct azioni{
+    game_info type; // specifico il tipo di azione
+    int player_id;
+    int target_id;
+    int x,y; // posizione nave da colpire
+} azioni;
