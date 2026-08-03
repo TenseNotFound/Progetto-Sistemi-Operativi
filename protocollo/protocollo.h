07b4ef22729f840dc155cae63bb1c1f614e3c1f4
LorenzoTarantino <lorenzotarantino40@gmail.com>
2026-08-03 18:19:31 +0200

    modifiche a protocollo.h: 1) aggiunta la voce MODE 2) inserimento struttura per rappresentare la nave 3) inserimento tipo di nave con dimensione 4) inserimento struttura per riportare il posizionamento di una nave nella griglia 5) define globale per la dimensione della griglia

--- protocollo/protocollo.h
+++ protocollo/protocollo.h
@@ -1,3 +1,6 @@
+#define SHIP_NUMBER 5
+#define GRID_SIZE 10
+
 typedef enum game_info{ // serve poi per capire che mossa/azione è stata fatta/compiuta/subita sul client
     WELCOME, JOIN, TURN, MOVE, HIT, MISS, ELIMINATED, WIN, MODE
 }game_info;
@@ -9,3 +12,22 @@ typedef struct azioni{
     int target_id;
     int x,y; // posizione nave da colpire
 } azioni;
+
+typedef struct navi{
+    const char *name;
+    int size;
+} nave;
+
+static const nave ship_tipe[SHIP_NUMBER] = {
+    {"Portaerei", 5 },
+    {"Corazzata", 4 },
+    {"Incrociatore", 3},
+    {"Sottomarino", 3},
+    {"Cacciatorpediniere", 2}
+};
+
+typedef struct posizionamento{
+    int index; //indice in ship_tipe
+    int x,y;
+    int orientamento; // 1 -> orizzontale, 0 verticale 
+}posizionamento;
